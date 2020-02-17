6a19661c9755c4a224d03129364194a1ed1a0387
micajank <micajank@umich.edu>
2020-02-17 15:14:52 -0800

    Parts one thru three working, when you click a pokemon from the main page, it shows on the favorites page and is in the database

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
 
 // GET /pokemon - return a page with favorited Pokemon
+// -View: views/pokemon/index.ejs
+// -Purpose: Retrieve all favorited Pokemon and display them on the page
 router.get('/', function(req, res) {
-  // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll().then(function(pokemon) {
+    console.log(req.body.name);
+    console.log(pokemon);
+    res.json(pokemon);
+    
+  }).catch(err => {
+    console.log(err);
+    res.send("ERROR");
+  })
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
+// -The form for adding is already included on the main index page
+// -View: none (redirect to /pokemon)
+// -Purpose: Creates a new Pokemon and redirects back to /pokemon
 router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.findOrCreate({
+    where: {
+      name: req.body.name
+    }
+  }).then(function([pokemon, created]) {
+    console.log(`Successfully ${created ? "created" : "found"} ${pokemon.name}.`);
+    res.redirect("/pokemon");
+  }).catch(err => {
+    console.log(err);
+    console.log("ERROR");
+  })
 });
 
 module.exports = router;
