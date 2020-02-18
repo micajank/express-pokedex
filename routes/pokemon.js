e0f27cad5a699c0478760c9901212afcbd805189
micajank <micajank@umich.edu>
2020-02-17 20:29:20 -0800

    Parts 1-4 working

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var axios = require('axios');
 var router = express.Router();
 var db = require('../models');
 
@@ -7,9 +8,7 @@ var db = require('../models');
 // -Purpose: Retrieve all favorited Pokemon and display them on the page
 router.get('/', function(req, res) {
   db.pokemon.findAll().then(function(pokemon) {
-    console.log(req.body.name);
-    console.log(pokemon);
-    res.json(pokemon);
+    res.render("../views/pokemon/index.ejs", { pokemon: pokemon});
     
   }).catch(err => {
     console.log(err);
@@ -27,12 +26,31 @@ router.post('/', function(req, res) {
       name: req.body.name
     }
   }).then(function([pokemon, created]) {
-    console.log(`Successfully ${created ? "created" : "found"} ${pokemon.name}.`);
     res.redirect("/pokemon");
   }).catch(err => {
     console.log(err);
     console.log("ERROR");
   })
 });
+
+router.get("/:name", function(req, res) {
+  var qs = {
+    params: {
+      s: req.query.name
+      //d: req.query.abitlities
+    }
+  }
+  axios.get(`http://pokeapi.co/api/v2/pokemon/${req.query.name}/`, qs)
+      .then(function (response) {
+        var results = response.data;
+        res.render("../views/pokemon/details.ejs", { pokemon: results });
+      })
+      .catch(function (err) {
+        console.log(`Error was made:\n ${err}`);
+      })
+      .finally(function() {
+        console.log("Made it through okay!");
+      });
+})
 
 module.exports = router;
